050e1c49aca19d7fbef7f0751ec4c939f7278056
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-04-21 14:38:52 +0900

    feat: block chain sketch

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -7,9 +7,9 @@ import (
 )
 
 type block struct {
-	data string
-	hash string
-	prevHash string
+	Data     string
+	Hash     string
+	PrevHash string
 }
 
 type blockchain struct {
@@ -19,33 +19,41 @@ type blockchain struct {
 var b *blockchain
 var once sync.Once
 
+func initBlockchain() {
+	if b == nil {
+		b = &blockchain{}
+		genesisBlock := createBlock("Genesis Block")
+		b.block = append(b.block, genesisBlock)
+	}
+}
 
 func (b *block) calculateHash() {
-	hash := sha256.Sum256([]byte(b.data + b.prevHash))
-	b.hash = fmt.Sprintf("%x", hash)
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash)
 }
 
 func getLastHash() string {
-	totalBlocks := len(GetBlockchain().block)
-	if totalBlocks == 0 {
+	if b == nil || len(b.block) == 0 {
 		return ""
 	}
-	return GetBlockchain().block[totalBlocks-1].hash
+	return b.block[len(b.block)-1].Hash
 }
 
-func createBlock(data string) *block {
-	newBlock := block{data, "", getLastHash()}
+func createBlock(Data string) *block {
+	newBlock := block{Data, "", getLastHash()}
 	newBlock.calculateHash()
 	return &newBlock
 }
 
-func GetBlockchain() *blockchain {
-	once.Do(func(){
-		if b == nil {
-			b = &blockchain{}
-			b.block = append(b.block, createBlock("Genesis Block"))
-		}
-	})
+func (b *blockchain) AddBlock(Data string) {
+	b.block = append(b.block, createBlock(Data))
+}
 
+func GetBlockchain() *blockchain {
+	once.Do(initBlockchain)
 	return b
+}
+
+func (b *blockchain) AllBlocks() []*block {
+	return b.block
 }
--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "github.com/nomadcoders/nomadcoin/blockchain"
+import (
+	"fmt"
+
+	"github.com/nomadcoders/nomadcoin/blockchain"
+)
 
 func main(){
 	chain := blockchain.GetBlockchain()
-
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("PrevHash: %s\n\n", block.PrevHash)
+	}
 }
