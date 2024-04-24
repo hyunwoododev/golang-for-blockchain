5826fed4946a1a20e2409bb257d221e5b0e97872
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-04-24 19:33:26 +0900

    feat: form on golang

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 	"github.com/nomadcoders/nomadcoin/blockchain"
 )
 
-const  (
-	port string = ":4000"
+const (
+	port        string = ":4000"
 	templateDir string = "templates/"
 )
 
@@ -18,18 +18,31 @@ var templates *template.Template
 
 type homeData struct {
 	PageTitle string
-	Blocks []*blockchain.Block
+	Blocks    []*blockchain.Block
 }
 
-func home(rw http.ResponseWriter, r *http.Request){
+func home(rw http.ResponseWriter, r *http.Request) {
 	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
 	templates.ExecuteTemplate(rw, "home", data)
 }
 
-func main(){
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockchain().AddBlock(data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	}
+}
+
+func main() {
 	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
 	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
 	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
