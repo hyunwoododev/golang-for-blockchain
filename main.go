6621176683ce71ddb7deb439275bbc76133d6873
dohyunwoo-gint <hyunwoo.do@gintlab.com>
2024-10-15 17:34:56 +0900

    marshal package

--- main.go
+++ main.go
@@ -1,4 +1,42 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port string =":4000"
+
+type URLDescription struct {
+	URL string `json:"url"`
+	Method string `json:"method"`
+	Description string `json:"description"`
+	Payload string `json:"payload,omitempty"` // omitempty는 optional로 바꿔줌
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request){
+	data := []URLDescription{
+		{
+			URL: "/",
+			Method: "GET",
+			Description: "See Documentation",
+		},
+		{
+			URL: "/blocks",
+			Method: "POST",
+			Description: "Add A Block",
+			Payload:"data:string",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(data)
+}
+
 func main() {
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port,nil))
+
 }
